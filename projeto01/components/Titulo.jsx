43a6ef6ab27049815b19359766c58b6e9aa5d251
Rafael Bomfim <rafinhapbomfim@gmail.com>
2022-08-29 20:14:57 -0300

    Utilizando o fragment

--- projeto01/components/Titulo.jsx
+++ projeto01/components/Titulo.jsx
@@ -1,8 +1,8 @@
 export default function Titulo(props){
     return (
-        <div>
+        <>
             <h1>{props.titulo}</h1>
             <h2>{props.subtitulo}</h2>
-        </div>
+        </>
     )
 }
--- projeto01/pages/exemplo04.jsx
+++ projeto01/pages/exemplo04.jsx
@@ -4,9 +4,9 @@ import Titulo from "../components/Titulo";
 
 export default function init(){
     return (
-        <div>
+        <>
             <Titulo titulo="Teste titulo" subtitulo="Teste subtitulo"></Titulo>
             <Titulo titulo="Pagina Cadastro" subtitulo="Gerenciar cadastro"/>
-        </div>
+        </>
     )
 }
