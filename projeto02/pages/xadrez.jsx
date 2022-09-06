6d9c462f1bab73744b60b8deb40e39688d6316dd
Rafael Bomfim <rafinhapbomfim@gmail.com>
2022-09-05 22:19:57 -0300

    Criando um tabuleiro

---
+++ projeto02/pages/xadrez.jsx
@@ -0,0 +1,11 @@
+import Tabuleiro from "../components/tabuleiro"
+
+export default function init(){
+    return (
+        <>
+            <Tabuleiro align-items="center"/>
+            <h1 >Outro Tabuleiro</h1>
+            <Tabuleiro/>  
+        </>
+    )
+}
