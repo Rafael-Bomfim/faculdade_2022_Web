4eab979c4945a012863128436e0729ac81a4348d
Rafael Bomfim <rafinhapbomfim@gmail.com>
2022-09-05 21:35:53 -0300

    Mais exemplos de css

---
+++ projeto02/pages/exemplo04.jsx
@@ -0,0 +1,10 @@
+import Apelido from "../components/Estilo";
+
+export default function init(){
+    return (
+        <>
+            <Apelido numero={10}></Apelido>
+            <Apelido numero={-5} direita/>
+        </>
+    )
+}
