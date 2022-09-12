440f0df26dd35c7d5f421adf70cf0542cdf5370e
Rafael Bomfim <rafinhapbomfim@gmail.com>
2022-09-12 20:41:24 -0300

    Renderizar listas

---
+++ projeto04/data/listaProdutos.js
@@ -0,0 +1,8 @@
+import Produto from "..//model/produto"
+
+export default [
+    new Produto(1, "Teclado", 80),
+    new Produto(2, "Mouse", 70),
+    new Produto(3, "Monitor", 100),
+    new Produto(4, "HD", 150)
+]
---
+++ projeto04/pages/exemplo02.jsx
@@ -0,0 +1,20 @@
+import listaProdutos from "../data/listaProdutos";
+
+export default function init(){
+
+    function renderizarLista(){
+        return listaProdutos.map(el => <li key={el.id}>
+            id = {el.id}
+            {" "}nome = {el.nome}
+            {" "}preco = {el.preco}
+            </li>)
+    }
+        return (
+            <>
+            <h1>Produtos</h1>
+            <ol>
+                {renderizarLista()}
+            </ol>
+            </>
+        )
+}
