432ca8827e67d4512624ad1eaddb2383b9f9cd39
Rafael Bomfim <rafinhapbomfim@gmail.com>
2022-10-17 21:27:43 -0300

    Projeto para trabalhar com framework tailwind

---
+++ projeto07/pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-r from-green-400 to-blue-500 h-screen flex justify-center 
+    items-center">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
